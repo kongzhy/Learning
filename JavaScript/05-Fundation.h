efb92b87f98d700f6c4f399215f4e724978c478f
孔朝阳 <kongzhaoyang@jd.com>
2016-04-27 10:51:03 +0800

    Init Git

---
+++ JavaScript/05-Fundation.h
@@ -0,0 +1,31 @@
+// JavaScript把null、undefined、0、NaN和空字符串''视为false，其他值一概视为true
+
+// Map
+// 由于一个key只能对应一个value，所以，多次对一个key放入value，后面的值会把前面的值冲掉
+
+// 添加新的key-value
+set()
+
+// 判断是否存在key-value
+has()
+
+// 获取value
+get()
+
+// 删除key-value
+delete()
+
+
+// Set
+
+// 添加元素
+add()
+
+// 删除元素
+delete()
+
+
+// for
+// for...in
+// for...of
+// forEach()
---
+++ Python/01-Introduce.h
@@ -0,0 +1,12 @@
+// 进入Python交互式环境
+python
+python3
+
+// 退出Python交互式环境
+exit()
+
+// 输出
+print()
+
+// 命令行模式下执行py
+python test.py
