26594c4054a9bbcde62828d5baf99af7cb5da480
Conway <kongzhoayang77@icloud.com>
2016-06-07 22:31:23 +0800

    HTTP

---
+++ HTTP/01-Network.h
@@ -0,0 +1,32 @@
+1.1、Web
+URL (Uniform Resource Locator)
+Client-Server
+
+1.2、HTTP(HyperText Transfer Protocol)
+WWW (World Wide Web)
+
+1.3、TCP/IP
+应用层:FTP(File Transfer Protocol)、DNS(Domain Name System)、HTTP(HyperText Transfer Protocol)
+传输层:TCP(Transmission Control Protocol)、UDP(User Data Protocol)
+网络层:IP(Internet Protocol)
+数据链路层
+
+1.4、IP、TCP
+
+1.4.1、IP
+IP Address？
+Media Access Control Address？
+ARP（Address Resolution Protocol)
+Routing?
+
+1.4.2、TCP
+Three-way handshaking
+SYN(Synchronize)
+ACK(Ackonwledgement)
+
+1.5、DNS
+提供域名到IP地址之间的解析服务
+
+1.6、Relation
+
+1.7、URI&URL
