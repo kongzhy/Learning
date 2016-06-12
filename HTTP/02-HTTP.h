6289b5f37039d888d06963bfcfab445d7bfd7c9a
Conway <kongzhoayang77@icloud.com>
2016-06-12 23:53:33 +0800

    HTTP：HTTP

--- HTTP/02-HTTP.h
+++ HTTP/02-HTTP.h
@@ -1,1 +1,23 @@
-// HTTP (HyperText Transfer Protocal)
+2 HTTP (HyperText Transfer Protocal)
+
+2.1、Comunication
+应用HTTP协议时，必定是一端担任客户端角色，另一端担任服务端角色
+
+2.2、Exchange
+请求必定由客户端发出，而服务端回复响应
+
+2.3、Stateless
+HTTP是一种无状态协议
+
+2.4、URI
+
+2.5、Method
+GET:获取资源
+POST:传输实体主体
+PUT:传输文件
+HEAD:获得报文首部
+DELETE:删除文件
+OPTIONAL:询问支持的方法
+TRACE:追踪路径
+CONNECT:要求用隧道协议连接代理
+
