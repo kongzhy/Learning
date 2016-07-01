3c3f963b5d19a2590d65a6aa149f232d7e60d78b
孔朝阳 <kongzhaoyang@jd.com>
2016-07-01 16:21:43 +0800

    JS：JSON

---
+++ JavaScript/15-JSON.h
@@ -0,0 +1,5 @@
+// JSON是JavaScript Object Notation的缩写，它是一种数据交换格式。
+// JSON还定死了字符集必须是UTF-8，表示多语言就没有问题了。
+// 为了统一解析，JSON的字符串规定必须用双引号""，Object的键也必须用双引号""。
+// 序列化，JSON.stringify()，要输出得好看一些，可以加上参数，按缩进输出：JSON.stringify(xiaoming, null, '  ');
+// 反序列化，JSON.parse()
