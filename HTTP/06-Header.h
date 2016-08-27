6d48c0c67a0bd73cb611ca5c0eb15f112c4bd01e
孔朝阳 <kongzhaoyang@jd.com>
2016-08-27 17:35:28 +0800

    Header

---
+++ HTTP/06-Header.h
@@ -0,0 +1,12 @@
+6.1、HTTP Header
+Request Header
+Response Header
+
+6.2、Header Fields
+General Header Fields
+Request Header Fields
+Response Header Fields
+Entity Header Fields
+
+6.3、General Header Fields
+Cache-Control:控制缓存的行为
