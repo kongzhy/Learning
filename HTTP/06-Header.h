0aeebf0af9581681c98c1540a2e882b52cb1a0d5
孔朝阳 <kongzhaoyang@jd.com>
2016-09-06 19:47:27 +0800

    Update

--- HTTP/06-Header.h
+++ HTTP/06-Header.h
@@ -39,3 +39,26 @@ Range:实体的字节范围请求
 Referer:对请求中URI的原始获取方
 TE:传输编码的优先级
 User-Agent:客户端程序的信息
+
+6.5、Response Header Fields
+Accept-Ranges:是否接受范围字节请求
+Age:推算资源创建经过时间
+ETag:资源的匹配信息
+Location:令客户端重定向至指定URI
+Proxy-Authenticate:代理服务器对客户端的认证信息
+Retry-After:对再次发起请求的时机要求
+Server:HTTP服务器的安装信息
+Vary:代理服务器缓存的管理信息
+WWW-Authenticate:服务器对客户端的认证信息
+
+6.6、Entity Header Fields
+Allow:资源可支持的HTTP方法
+Content-Encoding:实体主体适用的编码方法
+Content-Language:实体主体的自然语言
+Content-Length:实体主体的大小
+Content-Location:替代对应资源的URI
+Content-MD5:实体主体的报文摘要
+Content-Range:实体主体的位置范围
+Content-Type:实体主体的媒体类型
+Expires:实体主体过期的日期时间
+Last-Modified:资源的最后修改日期时间
