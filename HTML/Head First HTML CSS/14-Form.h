66a1e1920abfbead13dfcfb0cc40a72f6fd42e06
孔朝阳 <kongzhaoyang@jd.com>
2017-05-10 18:30:32 +0800

    Update

---
+++ HTML/Head First HTML CSS/14-Form.h
@@ -0,0 +1,16 @@
+<input type="">
+text:文本输入
+submit:提交输入
+radio:单选框
+checkbox:复选框
+
+number:数字输入(value、min、max)
+range:范围输入(value、min、max、step)
+color:颜色输入
+date:日期输入
+email:
+tel:
+url:
+
+<textarea>:文本区
+<select>、<option>:选择菜单
