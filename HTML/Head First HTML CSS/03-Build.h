ceebd5d0bd17cce6b13bcdae7878fb966cdc8279
孔朝阳 <kongzhaoyang@jd.com>
2017-05-11 12:12:22 +0800

    Update

--- HTML/Head First HTML CSS/03-Build.h
+++ HTML/Head First HTML CSS/03-Build.h
@@ -1,5 +1,5 @@
 <h1>、<p>、<blockquote>、<ol>、<ul>、<li>都是块元素，单独显示，在内容前后分别有一个换行
-<a>、<q>、<em>、<img>是内联元素，内容与其他内容一起显示
+<a>、<q>、<em>、<img>、<input>是内联元素，内容与其他内容一起显示
 块元素特立独行、内联元素随波逐流。
 void元素没有内容(确切的讲，void元素是在开始标记和结束标记之间没有任何内容的元素)，只有一个标记组成
 ol(ordered list) ul(unordered list) li(list item)
--- HTML/Head First HTML CSS/14-Form.h
+++ HTML/Head First HTML CSS/14-Form.h
@@ -1,16 +1,25 @@
+<form>元素定义了表单，所有表单输入元素都嵌套在<form>中
+action属性包含服务器脚本的URL
+method属性包含发送表单数据的方法，POST或GET
+
 <input type="">
-text:文本输入
-submit:提交输入
-radio:单选框
-checkbox:复选框
+text:单行文本输入框(name、value、maxlength、placeholder、required)
+submit:提交按钮
+radio:单选框(checked)
+checkbox:复选框(checked)
 
 number:数字输入(value、min、max)
-range:范围输入(value、min、max、step)
-color:颜色输入
-date:日期输入
+range:范围滑动控件(value、min、max、step)
+color:颜色选择器
+date:日期选择器
 email:
 tel:
 url:
+file:
 
-<textarea>:文本区
-<select>、<option>:选择菜单
+<textarea>:多行文本输入区(width、height)
+<select>:选择菜单(multiple)
+<option>:
+<label>:标签(for)
+<fieldset>、<legend>
+<passwords>
