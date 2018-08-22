6d5a2e9ea486b0815134b6eb5de60a518e115bb2
Conway <kongzhoayang77@icloud.com>
2018-08-22 22:15:32 +0800

    update

---
+++ Python/04-List.h
@@ -0,0 +1,18 @@
+// List
+
+// 获得List元素的个数
+len(list)
+
+// 使用索引访问List中相应的元素，可以使用-1作为索引，直接获取最后一个元素的索引
+List[-1]
+
+// 追加元素到List末尾
+append(element)
+
+// 插入元素到List指定位置
+insert(index, element)
+
+// 删除List末尾的元素，或删除指定位置的元素
+pop()
+
+
