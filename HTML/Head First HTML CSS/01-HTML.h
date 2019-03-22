39384e04eb59930fa0d0382e8c159668202e40f5
孔朝阳 <kongzhaoyang@jd.com>
2019-03-22 13:41:10 +0800

    delete HTML

--- HTML/Head First HTML CSS/01-HTML.h
+++
@@ -1,7 +0,0 @@
-HTML(HyperText Markup Language)，建立网页结构
-CSS(Cascading Style Sheets)，控制网页表现
-通过HTML，我们利用标记来标示内容提供结构
-元素=开始标记+内容+结束标记
-元素可以有属性，属性可以提供一个元素的附件信息
-所有页面都要有一个<html>元素，其中要有一个<head>元素和一个<body>元素
-<!--注释-->
